e0467398f6049c8cf600f07ec8e824ca0c23aaee
Thelyrebird1101 <59442877+Thelyrebird1101@users.noreply.github.com>
2020-04-14 11:25:42 +0530

    Update StringReversal.java

--- Data Structures and Algo - Zero to Mastery/src/com/arrays/StringReversal.java
+++ Data Structures and Algo - Zero to Mastery/src/com/arrays/StringReversal.java
@@ -6,7 +6,7 @@ public class StringReversal {
 				System.out.println("Given String is: "+ str);
 				System.out.println("Reversed String is: " + reverseString2(str));
 				}
-
+		// Using an array to reverse a string
 		private static String reverseString(String str) {
 			if(	str == null || str.length()<2 ) 
 					return "Not good enough";
@@ -18,7 +18,7 @@ public class StringReversal {
 			return reversedStr;
 		}
 		
-		
+		//Using StringBuilder class of java, which has inbuilt reverse method
 		private static String reverseString2(String str) {
 			StringBuilder sb = new StringBuilder();
 			return sb.append(str).reverse().toString();	

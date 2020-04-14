9fa101d9ed8e2622d812aec941fd1f272653ec23
aksthaku <aksthaku@WKWIN5535774.global.publicisgroupe.net>
2020-04-14 12:11:41 +0530

    Program to merge sorted arrays.

--- Data Structures and Algo - Zero to Mastery/src/com/arrays/MergeSortedArray.java
+++ Data Structures and Algo - Zero to Mastery/src/com/arrays/MergeSortedArray.java
@@ -12,22 +12,22 @@ public class MergeSortedArray {
 	}
 	
 	private static int[] mergeSorted(int[] arr1, int[] arr2) {
-		int[] arr3  = new int[arr1.length + arr2.length]; 
+		int[] arr3  = new int[arr1.length + arr2.length]; // Create a new array of length sum of sizes of the two arrays
 		int i=0, j=0, k=0;
 		while(i<arr1.length && j<arr2.length) {
-			if(arr1[i]<arr2[j])
+			if(arr1[i]<arr2[j])      // Add smaller element  to the array
 					arr3[k++] = arr1[i++];
 			else 
 					arr3[k++] = arr2[j++];
 		}
 		while(i<arr1.length)
-			arr3[k++] = arr1[i++];
+			arr3[k++] = arr1[i++];  // Add any remaining element of either arrays 
 	
 		while(j<arr2.length)
 			arr3[k++] = arr1[j++];
 		
 		
-		return arr3;
+		return arr3; 
 	}
 
 }

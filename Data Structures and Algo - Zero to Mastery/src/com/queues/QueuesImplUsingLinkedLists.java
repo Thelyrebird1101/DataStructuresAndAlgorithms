9796a6b8b72b632d2ca0504dda37fd26d5ae8cb2
aksthaku <aksthaku@WKWIN5535774.global.publicisgroupe.net>
2020-04-20 08:18:37 +0530

    Stacks and Queues

--- Data Structures and Algo - Zero to Mastery/src/com/queues/QueuesImplUsingLinkedLists.java
+++ Data Structures and Algo - Zero to Mastery/src/com/queues/QueuesImplUsingLinkedLists.java
@@ -1,5 +1,57 @@
 package com.queues;
 
 public class QueuesImplUsingLinkedLists {
-
+	Node front, rear;
+	class Node{
+		int data;
+		Node next;
+		Node(int x){
+			this.data = x;
+			this.next = null;
+		}
+	}
+	public QueuesImplUsingLinkedLists() {
+		this.front = null;
+		this.rear = null;
+	}
+	
+	public boolean isEmpty() {
+		return (front == null);
+	}
+	
+	public void enQueue(int x) {
+		Node newNode = new Node(x);
+		if(rear == null) {
+			rear = newNode;
+			rear.next = null;
+			front=rear;
+		}
+		else {
+			rear.next = newNode;
+			rear = rear.next;
+			rear.next = null;
+		}
+	}
+	
+	public void deQueue() {
+		if(isEmpty()) {
+			System.out.println("Queue is Empty");
+			return;
+		}
+		front = front.next; 
+	}
+	
+	public int front() {
+		if(!isEmpty())
+			return front.data;
+		else return Integer.MIN_VALUE;
+	}
+	
+	public int rear() {
+		if(!isEmpty())
+				return rear.data;
+		else return Integer.MIN_VALUE;
+	}
+	
+	
 }
--- Data Structures and Algo - Zero to Mastery/src/com/stacks/StacksImplUsingLinkedList.java
+++ Data Structures and Algo - Zero to Mastery/src/com/stacks/StacksImplUsingLinkedList.java
@@ -2,4 +2,60 @@ package com.stacks;
 
 public class StacksImplUsingLinkedList {
 
+		public class Node{
+			int data;
+			Node link;
+			
+		}
+		
+		Node top;
+		public StacksImplUsingLinkedList() {
+			this.top = null;
+		}
+		
+		public void push(int x) {
+			Node temp = new Node();
+			if(temp == null) {
+				System.out.print("heap overflow");
+				return;
+			}
+			temp.data = x;
+			temp.link = top;
+			top = temp;
+		}
+		
+		public boolean isEmpty() {
+			return top ==null;
+		}
+		
+		public int peek() {
+			if(!isEmpty())
+					return top.data;
+			else {
+				System.out.println("Stack is Empty");
+				return -1;
+			}
+		}
+		
+		public void pop() {
+			if(top == null) {
+				System.out.println("\nStack Underflow");
+				return;
+			}
+			
+			top = (top).link;
+		}
+		public void display() {
+			if(top == null) {
+				System.out.println("\nStack Underflow");
+				return;
+			}
+			else {
+				Node temp = top;
+				while(temp!=null) {
+					System.out.printf("%d ->", temp.data);
+					temp = temp.link;
+				}
+			}
+		}
 }
